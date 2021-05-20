bb45c7249fdae524bbe16fdb84f27d0ec261fb09
yujin0131 <65699860+yujin0131@users.noreply.github.com>
2021-05-20 20:47:34 +0900

    Create backend project
    Create backend project

---
+++ src/test/java/bis/stock/back/StockingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bis.stock.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
